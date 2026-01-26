0c462b30283a1ae207e7e0210383c420ed0f61dd
abdomg42 <sonysam3a@gmail.com>
2026-01-26 02:52:33 +0100

    add other model alternative for bert

--- models/bert_sentiment.py
+++ models/bert_sentiment.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
+# on peut utiliser un autre modèle plus rapide (cmarkea/distilcamembert-base-sentiment)
 class BertSentiment:
-    def __init__(self, model_id="tblard/tf-allocine"):
+    def __init__(self, model_id="tblard/tf-allocine"): 
         self.pipe = pipeline("sentiment-analysis", model=model_id)
 
     def analyze(self, text):
